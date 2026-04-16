7e92ba0e631281ff82d98b83aec4ea99c54d4002
Abdul Samad Gilal <abdulsgilal@gmail.com>
2026-04-16 14:02:55 +0500

    Update Group Anagrams.py

--- medium_neetCode_Array/Group Anagrams.py
+++ medium_neetCode_Array/Group Anagrams.py
@@ -1,2 +1,12 @@
-input_string = ["eat","tea","tan","ate","nat","bat"]
+from collections import defaultdict
 
+input_string = [""]
+
+def groupAnagram(stringList):
+    result = defaultdict(list)
+    for word in stringList:
+        key = tuple(sorted(word))
+        result[key].append(word)
+    return list(result.values())
+
+print(groupAnagram(input_string))
