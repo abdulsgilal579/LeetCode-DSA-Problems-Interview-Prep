7598073aae745e718c98686a64aff94879d21f2d
Abdul Samad Gilal <abdulsgilal@gmail.com>
2026-04-17 13:16:27 +0500

    Create 347. Top K Frequent Elements.py

---
+++ medium_neetCode_Array/347. Top K Frequent Elements.py
@@ -0,0 +1,10 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        sets = {}
+        for number in nums:
+            if number in sets:
+                sets[number] +=1
+            else:
+                sets[number] = 1
+        asc = {k: v for k, v in sorted(sets.items(), key=lambda item: item[1], reverse=True)}
+        return list(asc.keys())[:k]
