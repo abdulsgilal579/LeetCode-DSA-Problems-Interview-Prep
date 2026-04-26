00704afb718e316ea6340061e624e2efaa0d3a32
Abdul Samad Gilal <abdulsgilal@gmail.com>
2026-04-26 15:20:34 +0500

    Update 128_Longest_Consecutive_Sequence.py

--- medium_neetCode_Array/128_Longest_Consecutive_Sequence.py
+++ medium_neetCode_Array/128_Longest_Consecutive_Sequence.py
@@ -1,5 +1,4 @@
 #Brute_Force_Approach
-from http.cookiejar import cut_port_re
 
 nums = [100,4,200,1,3,2]
 
@@ -7,18 +6,16 @@ def longestConsecutiveSequence(nums):
     sortedArray = sorted(nums)
     currentStreak = 1
     bestStreak = 1
-    for i in range(0,len(sortedArray)-1):
-        if sortedArray[i+1] - sortedArray[i] == 1:
-            currentStreak +=1
+    if len(nums) == 0:
+        return 0
+    for i in range(0, len(sortedArray) - 1):
+        if sortedArray[i + 1] - sortedArray[i] == 1:
+            currentStreak += 1
             bestStreak = max(currentStreak, bestStreak)
-        elif sortedArray[i+1] - sortedArray[i] == 0:
+        elif sortedArray[i + 1] - sortedArray[i] == 0:
             continue
         else:
-            currentStreak =1
+            currentStreak = 1
     return bestStreak
 
-
-
-
-
-print(longestConsecutiveSequence(nums=nums))    
+print(longestConsecutiveSequence(nums=nums))
