23c7cef465cbfe91fa11bd4269eacbd08bcd02a9
Abdul Samad Gilal <abdulsgilal@gmail.com>
2026-04-26 14:02:34 +0500

    Update 128_Longest_Consecutive_Sequence.py

--- medium_neetCode_Array/128_Longest_Consecutive_Sequence.py
+++ medium_neetCode_Array/128_Longest_Consecutive_Sequence.py
@@ -1,1 +1,14 @@
-#Will solve this tomorrow between asar - maghrib prayer
+#BruteForce Approach
+
+nums = [100,4,200,1,3,2]
+
+def longestConsecutiveSequence(nums):
+    sortedArray = sorted(nums)
+    difference = nums[1] - nums[0]
+    for i in range(0, len(nums)):
+        if nums[i+1] - nums[i] != 
+
+
+
+
+print(longestConsecutiveSequence(nums=nums))
