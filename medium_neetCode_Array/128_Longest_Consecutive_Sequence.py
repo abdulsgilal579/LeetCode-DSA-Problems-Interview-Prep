d4dd3241c942c4dcde0e6d2bf98a37d970a2a073
Abdul Samad Gilal <abdulsgilal@gmail.com>
2026-04-26 15:11:19 +0500

    Bruteforce Implementation

--- medium_neetCode_Array/128_Longest_Consecutive_Sequence.py
+++ medium_neetCode_Array/128_Longest_Consecutive_Sequence.py
@@ -1,14 +1,24 @@
-#BruteForce Approach
+#Brute_Force_Approach
+from http.cookiejar import cut_port_re
 
 nums = [100,4,200,1,3,2]
 
 def longestConsecutiveSequence(nums):
     sortedArray = sorted(nums)
-    difference = nums[1] - nums[0]
-    for i in range(0, len(nums)):
-        if nums[i+1] - nums[i] != 
+    currentStreak = 1
+    bestStreak = 1
+    for i in range(0,len(sortedArray)-1):
+        if sortedArray[i+1] - sortedArray[i] == 1:
+            currentStreak +=1
+            bestStreak = max(currentStreak, bestStreak)
+        elif sortedArray[i+1] - sortedArray[i] == 0:
+            continue
+        else:
+            currentStreak =1
+    return bestStreak
 
 
 
 
-print(longestConsecutiveSequence(nums=nums))
+
+print(longestConsecutiveSequence(nums=nums))    
