250a72208261166277f08ebe84500aa423492d1e
Abdul Samad Gilal <abdulsgilal@gmail.com>
2026-04-26 17:07:24 +0500

    Optimal Solution

--- medium_neetCode_Array/128_Longest_Consecutive_Sequence.py
+++ medium_neetCode_Array/128_Longest_Consecutive_Sequence.py
@@ -1,21 +1,43 @@
 #Brute_Force_Approach
 
+# nums = [100,4,200,1,3,2]
+
+# def longestConsecutiveSequence(nums):
+#     sortedArray = sorted(nums)
+#     currentStreak = 1
+#     bestStreak = 1
+#     if len(nums) == 0:
+#         return 0
+#     for i in range(0, len(sortedArray) - 1):
+#         if sortedArray[i + 1] - sortedArray[i] == 1:
+#             currentStreak += 1
+#             bestStreak = max(currentStreak, bestStreak)
+#         elif sortedArray[i + 1] - sortedArray[i] == 0:
+#             continue
+#         else:
+#             currentStreak = 1
+#     return bestStreak
+
+# print(longestConsecutiveSequence(nums=nums))
+
+
+#----------------------------------------------
+#OPTIMAL SOLUTION
+#----------------------------------------------
+
 nums = [100,4,200,1,3,2]
 
-def longestConsecutiveSequence(nums):
-    sortedArray = sorted(nums)
-    currentStreak = 1
-    bestStreak = 1
-    if len(nums) == 0:
-        return 0
-    for i in range(0, len(sortedArray) - 1):
-        if sortedArray[i + 1] - sortedArray[i] == 1:
-            currentStreak += 1
-            bestStreak = max(currentStreak, bestStreak)
-        elif sortedArray[i + 1] - sortedArray[i] == 0:
-            continue
-        else:
-            currentStreak = 1
-    return bestStreak
+def longestStreak(nums):
+    numSet = set(nums)
+    result = 0
+    for number in nums:
+        if number - 1 not in numSet:
+            current_Streak = 1
+            while number + current_Streak in numSet:
+                current_Streak +=1
+            result = max(result, current_Streak)
+    return result
 
-print(longestConsecutiveSequence(nums=nums))
+print(longestStreak(nums))
+        
+
