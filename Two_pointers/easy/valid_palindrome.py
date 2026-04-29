d6c1a3249d5d1f42aa024b39b3a8b8a17e9f0df0
Abdul Samad Gilal <abdulsgilal@gmail.com>
2026-04-30 01:01:14 +0500

    Fixed Errors

--- Two_pointers/easy/valid_palindrome.py
+++ Two_pointers/easy/valid_palindrome.py
@@ -1,13 +1,16 @@
-string = "samad"
+string = "A man, a plan, a canal: Panama"
 def valid_palindrome(string):
     cleanedString = "".join([c.lower() for c in string if c.isalnum()])
     left = 0
     right = len(cleanedString)-1
     while left < right:
-        if cleanedString[left] == cleanedString[right]:
-            return True
+        if cleanedString[left] != cleanedString[right]:
+            return False
+        else:
             left += 1
             right -= 1
-        return False
+    
+    return True
+        
 
 print(valid_palindrome(string))
