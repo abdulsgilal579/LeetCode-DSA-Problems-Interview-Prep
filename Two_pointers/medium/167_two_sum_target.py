95230f4b33681437d51c84d896b28a1fa1abd97c
Abdul Samad Gilal <abdulsgilal@gmail.com>
2026-04-30 16:23:34 +0500

    167. Two Sum II - Input Array Is Sorted First Attempt

---
+++ Two_pointers/medium/167_two_sum_target.py
@@ -0,0 +1,19 @@
+from asyncio import current_task
+from os.path import curdir
+
+array = [-1,0]
+def twoSum(array, target):
+    left = 0
+    right = len(array)-1
+    while left < right:
+        currentSum = array[left] + array[right]
+        if currentSum == target:
+            return left+1, right+1
+        elif currentSum > target:
+            right -= 1
+        else:
+            left +=1
+    return "Indexes not found"
+
+print(twoSum(array, target=-1))
+
